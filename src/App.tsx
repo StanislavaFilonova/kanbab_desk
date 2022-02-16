2f915c6822c2fa7137420b406b94afb27cae3e23
Stanislava Filonova <stanislavafilonova@yandex.ru>
2022-02-16 23:58:05 +0300

    installing  TS with React

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello, TS!</p>
+                <a
+                    className="App-link"
+                    href="https://create-react-app.dev/docs/adding-typescript/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    How to install TS to your JS project
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
